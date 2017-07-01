403eb420f36500f001e0d7f3659b7036a2cba005
Ahmet KAYAR <ahmet.kayar@gmail.com>
2017-07-02 02:08:34 +0300

    Azur comment

--- WebInfoApp/App.Core/Constant/Connection.cs
+++ WebInfoApp/App.Core/Constant/Connection.cs
@@ -2,6 +2,7 @@
 {
     public static class Connection
     {
+        //Azur Comment
         private static string azure =
                 "Server=tcp:boyut.database.windows.net,1433;Initial Catalog=WebInfo;Persist Security Info=False;User ID=webinfo;Password=201408As;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30"
             ;
